8087c4183ee2b31f93fe7c8a143fdb262e50793c
AidanRJ1 <1aidanrj@gmail.com>
2025-02-26 12:07:39 +0000

    add function count character appearances in book

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_word_count, get_char_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,6 +8,8 @@ def get_book_text(filepath):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = get_word_count(book_text)
+    char_count = get_char_count(book_text)
     print(f"{word_count} words found in the document")
+    print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def get_word_count(book_text):
     book = book_text.split()
     word_count = len(book)
     return word_count
+
+def get_char_count(book_text):
+    book = book_text.lower()
+    char_count = {}
+    for char in book:
+        if char not in char_count:
+            char_count[char] = 1
+        else:
+            char_count[char] += 1
+    return char_count
