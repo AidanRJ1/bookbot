7c225e7faebdd7db0d462c9c0e2eb6cef5ed6d67
AidanRJ1 <1aidanrj@gmail.com>
2025-02-26 14:20:22 +0000

    add use of system arguments to choose book for analysis

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
     get_word_count, 
     get_char_count, 
@@ -10,7 +11,11 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    filepath = sys.argv[1]
     book_text = get_book_text(filepath)
     word_count = get_word_count(book_text)
     char_count = get_char_count(book_text)
