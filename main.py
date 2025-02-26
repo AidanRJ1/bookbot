61713041e819482c21da8a56520833889da70aa0
AidanRJ1 <1aidanrj@gmail.com>
2025-02-26 12:00:35 +0000

    refactor, split main.py and stats.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from stats import get_word_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
-
-def get_word_count(book_text):   
-    book = book_text.split()
-    word_count = len(book)
-    return word_count
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_word_count(book_text):
+    book = book_text.split()
+    word_count = len(book)
+    return word_count
